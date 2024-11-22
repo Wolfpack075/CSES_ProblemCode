3efd6a7fd1832c1937f9daa43f66a5a55648b400
Wolfpack075 <kazifahimtahmid@gmail.com>
2024-11-22 22:45:09 +0600

    gray code ber korar way is ager length er shob code er mirror add kora lage erpor first half er shamne 0 ar porer half er shamne 1 diye dile n+1 length er gray code produce hoy

---
+++ Introductory Problems/Gray Code.cpp
@@ -0,0 +1,132 @@
+/*
+    
+    Bismillahir Rahmanir Rahim. 
+    Every praise goes to ALLAH for our wellbeing.
+    I won't care for a single person who won't do the same for me.
+    Forgetting her was the best decision I took for myself.
+    Time is a relentless adversery. So, make sure you are looking after yourself. Family is everything. Keep them close.
+    One day I'll make you proud, mom. Stay strong.
+    Godspeed.
+    Author : Kazi Fahim Tahmid
+    Judge: CSES Introductory Problem ( Gray Code )
+    Date : Friday, 22 November 2024, 10:14PM
+ 
+    
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define int            long long int 
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+#define forr(x,n)      for(int i=x; i<n; i++)
+ 
+void flush(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+ 
+void file(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt",  "r",  stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+ 
+inline int power(int a, int b)
+{
+    int x = 1;
+    while (b)
+    {
+        if (b & 1) x *= a;
+        // cout << x << endl;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+ 
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+ 
+const int N = 200005;
+const int modd = 1e9+7;
+
+void fact(int n){
+    int x=1;
+    for(int i=2;i<=n;i++) x*=i;
+        cout << x;
+}
+    int arr[27];
+ 
+void solve() {
+    int n;cin>>n;
+    vector<string> vec;
+    vec.pb("0");
+    vec.pb("1");
+    // print(vec);
+    for(int i=2;i<=n;i++){
+        vector<string> vec2=vec;
+        // for(int i=0;i<sz(vec2);i++){
+        //     for(int j=0;j<vec2[i].length();j++){
+        //         if(vec2[i][j]=='0') vec2[i][j] = '1'; 
+        //         else vec2[i][j] = '0';
+        //     } 
+        // }
+        reverse(all(vec2));
+        vec.insert(vec.end(),vec2.begin(),vec2.end());
+        for(int i=0;i<sz(vec)/2;i++){
+            vec[i] = "0" + vec[i];
+            vec[sz(vec)-1-i] = "1"+ vec[sz(vec)-1-i];
+        }
+        
+    }
+    for(auto it:vec){
+        cout << it << endl;
+    }
+ 
+}
+ 
+int32_t main()
+{
+    
+    flush();
+    file();
+ 
+    
+    clock_t z = clock();
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+ 
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+ 
+    return 0;
+}
