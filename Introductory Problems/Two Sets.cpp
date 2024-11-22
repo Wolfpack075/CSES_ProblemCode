7d0f885ff5bed6196d7b7fc4343ff7c873816756
Wolfpack075 <kazifahimtahmid@gmail.com>
2024-11-22 17:41:34 +0600

    knight ar set er problem gula bhalo chilo

--- Introductory Problems/Two Sets.cpp
+++ Introductory Problems/Two Sets.cpp
@@ -4,20 +4,20 @@
     Every praise goes to ALLAH for our wellbeing.
     I won't care for a single person who won't do the same for me.
     Forgetting her was the best decision I took for myself.
-    Time is a relentless adversery. So, make sure you are looking after yourself. Family is everything. Keep them close.
-    One day I'll make you proud, mom. Stay strong.
-    Godspeed.
+	Time is a relentless adversery. So, make sure you are looking after yourself. Family is everything. Keep them close.
+	One day I'll make you proud, mom. Stay strong.
+	Godspeed.
     Author : Kazi Fahim Tahmid
-    Judge: CSES Introductory Problem ( Two Knights )
-    Date : Friday, 22 November 2024, 12:33PM
+    Judge: CSES Introductory Problem ( Two Sets )
+    Date : Friday, 22 November 2024, 04:20PM
  
     
 */
- 
+
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define int            long long int
 #define F              first
 #define S              second
@@ -40,19 +40,19 @@ using namespace std;
 #define print(a)       for(auto x : a) cout << x << " "; cout << endl
 #define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
 #define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
-#define forr(x,n)      for(int i=x; i<n; i++)
- 
+#define forr(x,n)	   for(int i=x; i<n; i++)
+
 void flush(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 }
- 
+
 void file(){
     #ifndef ONLINE_JUDGE
         freopen("input.txt",  "r",  stdin);
         freopen("output.txt", "w", stdout);
     #endif
 }
- 
+
 inline int power(int a, int b)
 {
     int x = 1;
@@ -64,7 +64,7 @@ inline int power(int a, int b)
     }
     return x;
 }
- 
+
 template <typename Arg1>
 void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
 template <typename Arg1, typename... Args>
@@ -73,32 +73,58 @@ void __f (const char* names, Arg1&& arg1, Args&&... args)
     const char* comma = strchr (names + 1, ',');
     cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
 }
- 
+
 const int N = 200005;
- 
+
 void solve() {
+
     int n;cin>>n;
-    // total number of ways hocche k bhabe 1no. knight place kora possible
-    // k-1 way te 2nd ta. but identical so kc2 korsi arki.
-    // 2x3 ar 3x2 jotogula block ase totogulay shomossha. per type er block e abar duibhabe attack kora jay. so 2(i-1)(i-2) + 2(i-2)(i-1)
-    for(int i=1;i<=n;i++) cout << (i*i * ( i*i-1) /2 ) - 4 * (i-1) * (i-2) << endl;
- 
+    // int odd_element = (n+1)/2;
+    // if(!(odd_element%2)){
+    //     int hudai = odd_element/2;
+    //     if(hudai%2==0 )
+    // }
+    if(n%2==0){
+        if((n%4)!=0) cout << "NO";
+        else {
+        	cout <<"YES" << endl;
+            cout << n/2 << endl;
+            for(int i=1;i<=n/2;i+=2) cout << i <<" "<< n+1-i << " ";
+            cout <<endl << n/2 << endl;
+            for(int i=2;i<=n/2;i+=2) cout << i << " " << n+1-i << " ";
+        }
+    }
+    else {
+    	int hudai = (n+1)/2;
+    	if(hudai%2!=0) cout << "NO";
+    	else {
+    		cout << "YES" << endl;
+    		hudai /=2;
+    		cout << n/2+1 << endl<< 1 << " ";
+    		for(int i=1;i<=hudai;i++) cout << 2*i << " ";
+    		for(int i=1;i<=(n/2-hudai);i++) cout << n-(2*i)+2 << " ";
+    		cout << endl << n/2 << endl;
+    		for(int i=1;i<=hudai;i++) cout << 2*i+1 << " ";
+    		for(int i=1;i<=(n/2-hudai);i++) cout << n-2*i+1 << " ";
+    	}
+    }
+
 }
- 
+
 int32_t main()
 {
     
     flush();
     file();
- 
+
     
     clock_t z = clock();
- 
+
     int t = 1;
     // cin >> t;
     while (t--) solve();
- 
+
     cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
- 
+
     return 0;
 }
